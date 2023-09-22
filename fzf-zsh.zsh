bee04eef5f67c439151fbbfb03b7df9677a92dfb
Kyle Hutchens <kyle@Kyles-MacBook-Pro-820.local>
2023-09-21 17:05:24 -0700

    Improve switch from .fzfignore to .bfsopts

--- fzf-zsh.zsh
+++ fzf-zsh.zsh
@@ -4,21 +4,7 @@ add-zsh-hook chpwd chpwd_recent_dirs
 
 zstyle ':chpwd:*' recent-dirs-max 32
 
-function fzf_zsh__find_cmd() {
-    ftype=${ftype:='f'}
-    if (( $+commands[bfs] )); then
-        ignorefile='.fzfignore'
-        ignore=''
-        if [[ -f $ignorefile ]]; then
-            for line in $(cat $ignorefile); do
-                ignore+=" -exclude -path '$line'"
-            done
-        fi
-        eval "bfs -type $ftype -follow -maxdepth 12 $ignore 2>/dev/null"
-    else
-        eval "find ."
-    fi
-}
+export FZF_ZSH__FIND_CMD='bfs $(echo $(cat .bfsopts 2>/dev/null)) 2>/dev/null'
 
 function _fzf_zsh__list_parents() {
     while true
@@ -34,7 +20,7 @@ function _fzf_zsh__list_parents() {
 }
 
 function fzf_zsh__cd() {
-    local dir=$(ftype=d fzf_zsh__find_cmd | fzf +m --scheme=path)
+    local dir=$(eval $FZF_ZSH__FIND_CMD -type d | fzf +m --scheme=path)
     if [[ -n $dir ]]; then
         cd $dir
     fi
@@ -55,7 +41,7 @@ function fzf_zsh__cd_history() {
 }
 
 function fzf_zsh__edit() {
-    fzf_zsh__find_cmd | fzf -m --select-1 --exit-0 --scheme=path | xargs -o $EDITOR
+    eval $FZF_ZSH__FIND_CMD | fzf -m --select-1 --exit-0 --scheme=path | xargs -o $EDITOR
 }
 
 function fzf_zsh__exec_history() {
